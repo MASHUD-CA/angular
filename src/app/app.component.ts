821f24144cf21fca8710b5563199bd48700eb602
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-04-30 18:55:37 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+//Component decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+//Logic part
 export class AppComponent {
   title = 'angular';
 }
