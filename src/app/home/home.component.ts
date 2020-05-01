a8418abbf8e0033dab676965d9b02522cfe42349
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-05-01 00:21:12 -0400

    Checked whether a CSS class Home is pplied to given element update or not

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  h1Style: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  firstClick() {
+    // console.log('clicked');
+    this.h1Style = true;
   }
-
 }
